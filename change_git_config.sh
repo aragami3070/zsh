69a7461f9bc5a4ae84403752a44aaa82c4f40ce5
aragami3070 <smirnov17612@gmail.com>
2025-10-01 01:00:01 +0400

    Fix(change_git_config): path to ~/.gitconfig

--- change_git_config.sh
+++ change_git_config.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ "$(grep "$GH_USER" .gitconfig | sed "s/.*= //")" = "$GH_USER" ]; then
-  tmp=$(sed 's/name = '"$GH_USER"'/name = '"$BLYANDEX_USER"'/' .gitconfig | sed 's/email = '"$GH_EMAIL"'/email = '"$BLYANDEX_EMAIL"'/')
-  echo "$tmp" > .gitconfig
+if [ "$(grep "$GH_USER" ~/.gitconfig | sed "s/.*= //")" = "$GH_USER" ]; then
+  tmp=$(sed 's/name = '"$GH_USER"'/name = '"$BLYANDEX_USER"'/' ~/.gitconfig | sed 's/email = '"$GH_EMAIL"'/email = '"$BLYANDEX_EMAIL"'/')
+  echo "$tmp" > ~/.gitconfig
 else
-  tmp=$(sed 's/name = '"$BLYANDEX_USER"'/name = '"$GH_USER"'/' .gitconfig | sed 's/email = '"$BLYANDEX_EMAIL"'/email = '"$GH_EMAIL"'/')
-  echo "$tmp" > .gitconfig
+  tmp=$(sed 's/name = '"$BLYANDEX_USER"'/name = '"$GH_USER"'/' ~/.gitconfig | sed 's/email = '"$BLYANDEX_EMAIL"'/email = '"$GH_EMAIL"'/')
+  echo "$tmp" > ~/.gitconfig
 fi
 
